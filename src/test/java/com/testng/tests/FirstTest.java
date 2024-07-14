8dc9b5dfdeca36fe2ca83f181d192f50161aaf29
im-vidhyashankar <vinutesting7@gmail.com>
2024-07-15 04:55:42 +1000

    Updated selenium dependencies, chromedriver dependencies, and aded simple tests under First test class

--- src/test/java/com/testng/tests/FirstTest.java
+++ src/test/java/com/testng/tests/FirstTest.java
@@ -1,5 +1,37 @@
 package com.testng.tests;
 
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirstTest {
+	
+	@Test
+	public void TestGoogle() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("W3 Schools", Keys.ENTER);
+		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+	@Test
+	public void TestFacebook() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.name("email")).sendKeys("W3 Schools", Keys.ENTER);
+		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
+		driver.quit();
+	}
 
 }
